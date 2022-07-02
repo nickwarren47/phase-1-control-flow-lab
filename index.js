469b3ab8ef66793d91350df69c3afb4c93d17a11
Nick Warren <nickwarren47@gmail.com>
2022-07-02 17:26:00 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
+    if (distance <= 400){
+      return 'This one is on me!';
+    } else if (distance >= 400 && distance <= 2000){
+      return 'That will be twenty bucks.';
+    } else if (distance >= 2000 && distance <= 2500){
+      return 'I will gladly take your thirty bucks.';
+    } else if (distance > 2500){
+      return 'No can do.';
+    };
+};
+
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+    return "Thank you.";
+    default:
+      return 'Bye.';
+  }
   // Write your code here!
 }
